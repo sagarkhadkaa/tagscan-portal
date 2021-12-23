9c65779342ca5da07ff43fa46689a38a595a6fab
sagar <sagarkhadka2069@gmail.com>
2021-12-23 12:10:15 +0545

    Login Validation

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -4,16 +4,52 @@ import Input from "./common/input";
 class LoginForm extends React.Component {
     state = {
         account: { username: "", password: "" },
+        errors: {},
+    };
+    validate = () => {
+        const errors = {};
+        const { account } = this.state;
+        if (account.username.trim() === "")
+            errors.username = "Username is required";
+        if (account.password.trim() === "")
+            errors.password = "Password is required";
+
+        return Object.keys(errors).length === 0 ? null : errors;
+    };
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        const errors = this.validate();
+        // console.log(errors);
+        this.setState({ errors: errors || {} });
+        if (errors) return;
+        //Server Call
+        console.log("Submitted");
+    };
+    validateProperty = ({ name, value }) => {
+        if (name === "username") {
+            if (value.trim() === "") return "Username is required.";
+            //...
+        }
+        if (name === "password") {
+            if (value.trim() === "") return "Password is required.";
+            //...
+        }
     };
     handleChange = ({ currentTarget: input }) => {
+        const errors = { ...this.state.errors };
+        const errorMesssage = this.validateProperty(input);
+        if (errorMesssage) errors[input.name] = errorMesssage;
+        else delete errors[input.name];
+
         const account = { ...this.state.account };
         account[input.name] = input.value;
-        this.setState({ account });
+        this.setState({ account, errors });
     };
     render() {
-        const { account } = this.state;
+        const { account, errors } = this.state;
         return (
-            <div className="m-4">
+            <div className="my-4  d-flex flex-column align-items-center ">
                 <h1>Login</h1>
                 <form onSubmit={this.handleSubmit}>
                     <Input
@@ -21,12 +57,14 @@ class LoginForm extends React.Component {
                         value={account.username}
                         label="Username"
                         onChange={this.handleChange}
+                        error={errors.username}
                     />
                     <Input
                         name="password"
                         value={account.password}
                         label="Password"
                         onChange={this.handleChange}
+                        error={errors.password}
                     />
                     <button className="btn btn-primary mt-4">Login</button>
                 </form>
